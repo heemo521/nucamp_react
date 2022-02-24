c9ba861428421e4945a47071ed1309f9b5613e4e
Heemo Yang <heemo521@gmail.com>
2022-02-24 12:14:38 -0800

    Split & Combine Reducers

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import 'typeface-open-sans';
 import 'font-awesome/css/font-awesome.css';
 import 'bootstrap-social/bootstrap-social.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
     <React.StrictMode>
@@ -19,4 +18,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
